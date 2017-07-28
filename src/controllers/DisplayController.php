40569e9e67dc90d5e0d31dd1a033c03adb8d7a0c
Richard Phillips <richp10@gmail.com>
2017-07-28 20:52:22 +0100

    Remove deprecated safe mode condition

--- src/controllers/DisplayController.php
+++ src/controllers/DisplayController.php
@@ -14,10 +14,9 @@
         {
             parent::__construct($container);
 
-            // Prevent timeouts on large exports (non-safe mode only)
-            if (!ini_get('safe_mode')) {
-                set_time_limit(0);
-            }
+            // Prevent timeouts on large exports
+            set_time_limit(0);
+
         }
 
         public function render()
